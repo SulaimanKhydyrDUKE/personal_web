1844339821048b0ab94f81593fdf3a5b87ec624f
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-15 23:53:44 -0500

    Added derivatives

--- sulaiman.dev/app/private/page.tsx
+++ sulaiman.dev/app/private/page.tsx
@@ -6,6 +6,13 @@ export default async function MetricsPage() {
 
     const mycommits = await getCommitCount("SulaimanKhydyrDuke", from, to)
     const theircommits = await getCommitCount(THEIR_USERNAME, from, to)
+    const days =
+    (new Date(to).getTime() - new Date(from).getTime()) /
+    (1000 * 60 * 60 * 24)
+
+    const myRate = (mycommits / days).toFixed(2)
+    const theirRate = (theircommits / days).toFixed(2)
+
     return (
         <main>
             <h1 className="text-2xl font-bold mb-6">
@@ -13,6 +20,9 @@ export default async function MetricsPage() {
             </h1>
             <p>My commits: {mycommits}</p>
             <p>Their commits: {theircommits}</p>
+            <p>My avg commits/day: {myRate}</p>
+            <p>Their avg commits/day: {theirRate}</p>
+
             <section className="mt-12">
       <h2 className="text-xl font-semibold mb-4">
        GitHub Activity
@@ -60,6 +70,7 @@ async function getCommitCount(
             },
         }),
     })
+    
     const num = await response.json()
     console.log("TOKEN EXISTS:", Boolean(process.env.GITHUB_TOKEN))
     console.log(`GitHub response for ${username}:`, JSON.stringify(num, null, 2));
